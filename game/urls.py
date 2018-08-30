2a25d22aa0c81a5c9a63ce68c0b765e885c12017
Hindia <mahmmudhindia@gmail.com>
2018-08-30 15:43:36 +0300

    back end

--- game/urls.py
+++ game/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
 
     #/game/1/
     url(r'^(?P<shelfNum>[0-9]+)/$', views.detail, name='detail'),
+
+    #/game/1/3
+    url(r'^(?P<shelfNum>[0-9]+)/(?P<compartmentNum>[0-9]+)/$', views.moredetail, name='moredetail'),
 ]
--- game/views.py
+++ game/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from django.http import Http404
 from django.shortcuts import render
-from .models import Shelf
+from .models import Shelf, Compartment, Item
 
 
 # Create your views here.
@@ -17,3 +17,9 @@ def detail(request, shelfNum):
     except Shelf.DoesNotExist:
         raise Http404("Shelf doesnt exist")
     return render(request, 'game/detail.html', {'shelf': shelf})
+
+
+def moredetail(request, shelfNum, compartmentNum):
+        compartment = Compartment.objects.get(pk=compartmentNum)
+        shelf = Shelf.objects.get(pk=shelfNum)
+        return render(request, 'game/moredetail.html', {'compartment': compartment, 'shelf': shelf})
