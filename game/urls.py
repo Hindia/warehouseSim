4339745079ed95e780ae6dccd0912e6302e62940
hindia <mahmmudhindia@gmail.com>
2018-10-10 01:02:47 +0300

    restful

--- game/urls.py
+++ game/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
+from rest_framework import routers
 
 urlpatterns = [
     #/game/
@@ -10,4 +11,6 @@ urlpatterns = [
 
     #/game/1/3
     url(r'^(?P<shelfNum>[0-9]+)/(?P<compartmentNum>[0-9]+)/$', views.moredetail, name='moredetail'),
+
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
