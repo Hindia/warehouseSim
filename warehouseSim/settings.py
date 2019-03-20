cd635334e8f97dea02825d490a8f53e7206c66a2
hindia <mahmmudhindia@gmail.com>
2019-03-20 12:07:28 +0200

    whitenoiseupdate

--- warehouseSim/settings.py
+++ warehouseSim/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAdminUser',
