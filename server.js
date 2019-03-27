df363bf63065679ad7a6d31bb20c9ff0b587a935
hindia <mahmmudhindia@gmail.com>
2019-03-27 14:25:45 +0200

    expressserver2

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ const app = express();
 app.use(favicon(warehouseSim + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(warehouseSim));
+//static file declaration
 app.use(express.static(path.join(warehouseSim, 'build')));
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
-app.get('/*', function (req, res) {
+//build mode
+app.get('*', function (req, res) {
   res.sendFile(path.join(warehouseSim, 'build', 'index.html'));
 });
 
