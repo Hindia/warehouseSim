a764219900e2b7e77ba202834b53542a7b79b90b
hindia <mahmmudhindia@gmail.com>
2019-03-29 16:29:16 +0200

    testing

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ app.use(favicon(warehouseSim + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(warehouseSim));
 //static file declaration
-app.use(express.static(path.join(warehouseSim, 'src')));
+app.use(express.static(path.join(warehouseSim, 'build')));
 
 //build mode
 app.get('*', function (req, res) {
-  res.sendFile(path.join(warehouseSim, 'src', 'index.html'));
+  res.sendFile(path.join(warehouseSim, 'build', 'index.html'));
 });
 
 app.listen(port);
